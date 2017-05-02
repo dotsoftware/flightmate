e6797ed2e1f43804d6371688d981058a86453b7e
Martin Doleschel <dot@Martins-iMac.local>
2017-05-02 17:53:15 +0200

    lounge update

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -138,14 +138,19 @@ function ($scope, $stateParams) {
 
 }])
 
-.controller('loungesCtrl', ['$scope', '$stateParams', 'FlightService', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('loungesCtrl', ['$scope', '$stateParams', 'FlightService', 'ionicToast', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams, FlightService) {
+function ($scope, $stateParams, FlightService, ionicToast) {
   FlightService.getLounges(FlightService.getCurrentAirport()).success(function(data) {
       console.log(data);
       $scope.lounges = data.LoungeResource.Lounges.Lounge;
   });
+
+  $scope.showHint = function(text) {
+    ionicToast.show(text, 'bottom', false, 3000);
+
+  }
 
 }])
 
